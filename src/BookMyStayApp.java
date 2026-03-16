c39b14d23623f7d6f8f193c0e20eb94abccbb093
srlskrishna576-byte <srlskrishna576@gmail.com>
2026-03-16 15:07:09 +0530

    UC2 - Basic Room Types & Static Availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,18 +1,103 @@
+abstract class Room {
+
+    protected String roomType;
+    protected int beds;
+    protected int size;
+    protected double price;
+
+    // Constructor
+    public Room(String roomType, int beds, int size, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    // Method to display room details
+    public void displayRoomDetails() {
+        System.out.println("Room Type : " + roomType);
+        System.out.println("Beds      : " + beds);
+        System.out.println("Size      : " + size + " sq.ft");
+        System.out.println("Price     : $" + price);
+    }
+
+    // Abstract method
+    public abstract void roomInfo();
+}
+
+
+/* -------- Single Room Class -------- */
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super("Single Room", 1, 200, 100);
+    }
+
+    public void roomInfo() {
+        System.out.println("Ideal for one guest.");
+    }
+}
+
+
+/* -------- Double Room Class -------- */
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super("Double Room", 2, 350, 180);
+    }
+
+    public void roomInfo() {
+        System.out.println("Suitable for two guests.");
+    }
+}
+
+
+/* -------- Suite Room Class -------- */
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super("Suite Room", 3, 600, 350);
+    }
+
+    public void roomInfo() {
+        System.out.println("Luxury room with premium facilities.");
+    }
+}
+
 public class BookMyStayApp{
+    public static void main(String[] args) {
 
-        public static void main(String[] args) {
+        // Creating room objects (Polymorphism)
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
 
-            // Printing welcome message to the console
-            System.out.println("Welcome to the Hotel Booking System");
+        // Static availability variables
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
 
-            // Displaying application name and version
-            System.out.println("Application: Hotel Booking System");
-            System.out.println("Version: v1.0");
+        System.out.println("===== HOTEL ROOM AVAILABILITY =====\n");
 
-            // Informing user that the program has started successfully
-            System.out.println("System initialized successfully.");
+        // Display Single Room details
+        single.displayRoomDetails();
+        single.roomInfo();
+        System.out.println("Available Rooms: " + singleAvailable);
+        System.out.println("-----------------------------------");
 
-            // Program ends after printing the messages
-        }
+        // Display Double Room details
+        doubleRoom.displayRoomDetails();
+        doubleRoom.roomInfo();
+        System.out.println("Available Rooms: " + doubleAvailable);
+        System.out.println("-----------------------------------");
+
+        // Display Suite Room details
+        suite.displayRoomDetails();
+        suite.roomInfo();
+        System.out.println("Available Rooms: " + suiteAvailable);
+        System.out.println("-----------------------------------");
+
+        System.out.println("Application Terminated.");
+    }
 
 }
